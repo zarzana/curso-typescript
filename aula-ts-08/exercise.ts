2b33528dba928fdee513c63481787489a6e2502e
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-09-13 16:48:27 -0300

    aula 8

---
+++ aula-ts-08/exercise.ts
@@ -0,0 +1,28 @@
+type Game = {
+    id: number;
+    platform: {
+        id: number;
+        name: string;
+    };
+    title: string;
+    publisher: string;
+    launch?: string | Date;
+}
+
+const game: Game = {
+    id: 1,
+    platform: {
+        id: 1,
+        name: "Playstation"
+    },
+    title: "The Last of Us",
+    publisher: "Naughty Dog",
+    launch: "2013-06-14" // pode ser um Date. também pode ser vazio
+}
+
+const games: Game[] = [game];
+
+function play(game: Game) {
+    // runs the game
+    console.log(game);
+}
